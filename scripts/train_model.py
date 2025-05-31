8cf71e52b37cba6f4a2149ccee2f45ced2ecd075
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-05-31 12:41:50 +0330

    importing

---
+++ scripts/train_model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import joblib
+import os
